5e93338b569ccdf222912810b7072fab76a23f1f
nagumo <aki@nagumnet>
2020-10-11 12:59:32 +0900

    remove: unuse style

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,6 @@ import Sub from './atomic/Pages/Sub'
 import './App.scss'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-const useStyles = makeStyles({
-  appHheader: {
-    backgroundColor: '#282c34',
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 'calc(10px + 2vmin)',
-    color: 'white',
-  },
-  appLink: {
-    color: '#61dafb',
-  },
-})
 
 const App = () => {
   return (
